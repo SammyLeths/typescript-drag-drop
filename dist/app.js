6c26b4b05ea58e2463706fd2964440feca43dcf3
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-06-14 13:51:41 +0100

    typescript drag and drop project with webpack build

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-import { ProjectInput } from './components/project-input.js';
-import { ProjectList } from './components/project-list.js';
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
-//# sourceMappingURL=app.js.map
---
+++ webpack.config.prod.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+//const { CleanPlugin } = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    //clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
